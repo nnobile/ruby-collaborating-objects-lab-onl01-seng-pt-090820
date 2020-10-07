d1513cb7c9e2beb570182d9b7e4414bfa39dfa5f
Nick Nobile <nickcnobile@gmail.com>
2020-10-07 01:57:48 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,15 @@
 class Song
   attr_accessor :name, :artist
+  @@all = []
 
   def initialize(name)
     @name = name
   end
+
+  def self.all
+    @@all
+  end
+
 
   def self.new_by_filename(filename)
     split_name = filename.split(" - ")
