391e8b1a49b3a4449f1ca796c2f50ec59421db3b
Nick Nobile <nickcnobile@gmail.com>
2020-10-07 02:08:43 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,39 @@
+class Artist
+  attr_accessor :name
+  attr_reader :songs
+  @@all = []
+
+def initialize(name)
+  @name = name
+  @songs = []
+  @@all << self
+end
+
+def self.all
+  @@all
+end
+
+def add_song(song)
+  @songs << song
+end
+
+def save
+  @@all << self
+end
+
+def self.find_or_create_by_name(name)
+  self.all.detect {|artist| artist.name == name} || Artist.new(name)
+end
+
+def print_songs
+  @songs.each { |song| puts song.name}
+end
+
+end
+
+#song_one = Song.new("Rock With You")
+#artist = Artist.new("Michael Jackson")
+#puts artist.name
+#artist2 = Artist.new("")
+#artist2.name = "Prince"
+#puts artist2.name
